d7af4ac39cd7f7eefcce7a551780a6b6bc0ec7cf
PalmTreeDream <127060944+PalmTreeDream@users.noreply.github.com>
2023-03-20 22:41:10 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+import os
+
+app = Flask(__name__)
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+@app.route('/')
+def index():
+    return render_template('updated_index.html')
+
+@app.route('/generate_lyrics', methods=['POST'])
+def generate_lyrics():
+    prompt = request.form['prompt']
+    genre = request.form['genre']
+    keyword1 = request.form.get('keyword1', '')
+    keyword2 = request.form.get('keyword2', '')
+    keyword3 = request.form.get('keyword3', '')
+    structure = request.form['structure']
+
+    keywords = ', '.join([k for k in [keyword1, keyword2, keyword3] if k])
+    keywords_prompt = f" and include the keywords: {keywords}" if keywords else ""
+
+    if structure == 'full':
+        song_structure = "The song should have two verses and three choruses."
+    elif structure == 'verse':
+        song_structure = "Write a verse for the song."
+    elif structure == 'chorus':
+        song_structure = "Write a chorus for the song."
+    else:  # structure == 'bridge'
+        song_structure = "Write a bridge for the song."
+
+    prompt = f"Write a {genre} song with the theme: {prompt}{keywords_prompt}. {song_structure}"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+
+    lyrics = response.choices[0].text.strip()
+    return jsonify({'lyrics': lyrics})
+
+if __name__ == '__main__':
+    app.run(debug=True)
